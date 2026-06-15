d2b402cbf2be62a4cf1695dacaf550659225c77f
hitamarpereiras <hitamarsilvaoficial@gmail.com>
2026-06-15 08:39:20 -0300

    Atualizações no Settings

--- core/settings.py
+++ core/settings.py
@@ -20,9 +20,8 @@ DEBUG = True
 ALLOWED_HOSTS = [
     "gamesearch-nine.vercel.app",
     "spacegames.vps8317.panel.icontainer.cloud",
-    "localhost",
     "127.0.0.1",
-    "216.22.27.187:5000",
+    "216.22.27.187",
     ]
 
 CORS_ALLOW_ALL_ORIGINS = True

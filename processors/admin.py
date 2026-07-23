c1042eeda08fa20f41b00798a524d025daaa5293
hitamarpereias <hitamarsilvaoficial@gmail.com>
2026-07-23 19:09:36 -0300

    admin atualizado em processors

--- processors/admin.py
+++ processors/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from processors.models import Processor
 
-# Register your models here.
+
+@admin.register(Processor)
+class ProcessorAdmin(admin.AdminSite):
+    list_display= [
+        'id',
+        'manufacturer',
+        'modelo',
+        'generation',
+        'yaer',
+        'creadted_at'
+    ]
+    search_fields= [
+        'manufacturer',
+        'modelo',
+        'generation'
+    ]
+    
